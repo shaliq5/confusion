9e2363441ee1b1f78d5ab3dbc718e0560d50c695
Shaliq Nigal <shaliq5@gmail.com>
2021-01-29 16:06:53 +0530

    Components-2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponents'
 import './App.css';
+import { DISHES } from './shared/dishes';
 
-function App() {
 
-  return (
-    <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Confusion Site</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu />
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props); 
+
+    this.state={
+      dishes: DISHES
+    };
+  }
+  render() {
+    return (
+      <div>
+          <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">Confusion Site</NavbarBrand>
+            </div>
+          </Navbar>
+          <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  };
   
 }
 
