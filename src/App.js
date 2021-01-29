954a81796a5cdc92bff315a7662acf79461f6abe
Shaliq Nigal <shaliq5@gmail.com>
2021-01-29 12:05:47 +0530

    Components-1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponents'
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
+    <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Confusion Site</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
     </div>
   );
+  
 }
 
 export default App;
