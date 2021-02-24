368bce2925807adcd8c3da289eb120b6ab7b767d
Shaliq Nigal <shaliq5@gmail.com>
2021-02-24 15:13:27 +0530

    Uncontrolled Forms

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,20 +1,35 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand,Nav ,NavbarToggler, NavItem, Collapse ,Jumbotron} from 'reactstrap';
+import { Navbar, NavbarBrand,Nav ,NavbarToggler, NavItem, Collapse ,Jumbotron, Button, Modal, ModalBody, ModalHeader, Form, FormGroup, Input, Label} from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component{
     constructor(props){
         super(props);
         this.state ={
-            isNavOpen : false
+            isNavOpen : false ,
+            isModalOpen: false,
         };
         this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
     }
 
     toggleNav(){
         this.setState({
             isNavOpen: !this.state.isNavOpen
         });
+    }
+
+    toggleModal(){
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleLogin(event){
+        this.toggleModal();
+        alert("Username: "+ this.username.value + " Password: " + this.password.value + " Remember: "+ this.remember.checked)
+        event.preventDefault();
     }
 
     render() {
@@ -49,6 +64,13 @@ class Header extends Component{
                             </NavLink>
                         </NavItem>
                     </Nav>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <Button outline onClick={this.toggleModal}>
+                                <span className="fa fa-sign-in fa-lg" />Login
+                            </Button>
+                        </NavItem>
+                    </Nav>
                     </Collapse>
                     </div>
                 </Navbar>
@@ -64,6 +86,28 @@ class Header extends Component{
                         </div>
                     </div>
                 </Jumbotron>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label html="username">Username</Label>
+                                <Input type="text" id="username" name="username" innerRef={(input) =>this.username=input}/>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label html="password">Password</Label>
+                                <Input type="password" id="password" name="password" innerRef={(input) =>this.password=input}/>
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember" innerRef={(input) =>this.remember=input}/>
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" className="bg-primary">Login</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
             </React.Fragment>
         );
     }
