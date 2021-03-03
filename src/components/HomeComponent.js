ed53a2583106ef12b5786e9435207494bbf15517
Shaliq Nigal <shaliq5@gmail.com>
2021-03-03 20:05:02 +0530

    react-animation-components

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Card, CardImg, CardText ,CardTitle ,CardBody , CardSubtitle} from 'reactstrap';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseURl';
+import { FadeTransform } from 'react-animation-components';
 
 function RenderCard({item, isLoading,  errMess }) {
     if(isLoading){
@@ -17,6 +18,9 @@ function RenderCard({item, isLoading,  errMess }) {
     else
     {
     return(
+        <FadeTransform in transformProps={{
+            exitTransform: 'scale(0.5) translateY(-50%)'
+        }}>
         <Card>
             <CardImg src={baseUrl + item.image} alt={item.name} />
             <CardBody>
@@ -25,7 +29,7 @@ function RenderCard({item, isLoading,  errMess }) {
                 <CardText>{item.description}</CardText>
             </CardBody>
         </Card>
-
+        </FadeTransform>
     );
     }
 }
--- src/components/dishDetailsComponent.js
+++ src/components/dishDetailsComponent.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseURl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -16,6 +17,9 @@ const minLength = (len) => (val) => val && (val.length >= len);
         return(
             
         <div className="col-12 col-md-5 m-1">
+            <FadeTransform in transformProps={{
+            exitTransform: 'scale(0.5) translateY(-50%)'
+        }}>
             <Card>
                 <CardImg src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
@@ -23,6 +27,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
                     <CardText>{dish.description}</CardText>
                 </CardBody>
             </Card>
+            </FadeTransform>
         </div>
         
         );
@@ -32,11 +37,17 @@ const minLength = (len) => (val) => val && (val.length >= len);
         if(comments != null){
             const commentListItems = comments.map((comment) =>{
                 return(
+                    <Stagger in>
+                    
                     <li key={comment.id}>
+                    <Fade in>
                         <p>Rating - {comment.rating}</p>
                         <p>{comment.comment}</p>
                         <p>--{comment.author} , {new Intl.DateTimeFormat('en-US',{year : 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                    </li>   
+                        </Fade>
+                        </li>  
+                    
+                    </Stagger>
                 );
             });
     
